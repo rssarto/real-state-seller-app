cc831521645d6ad119809232e280f48bc5d2798f
Ricardo Sarto <ricardo.soares.sarto@gmail.com>
2019-03-26 20:28:51 -0300

    enabling cors

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,8 @@ insert into CLIENT_MODEL (ID, EMAIL, NAME, BORN_DATE, WAGE ) values (100001, 'ma
 insert into CLIENT_MODEL (ID, EMAIL, NAME, BORN_DATE, WAGE ) values (100002, 'william@teste.com.br', 'William', '1999-02-20', 13000)
 insert into CLIENT_MODEL (ID, EMAIL, NAME, BORN_DATE, WAGE ) values (100003, 'carolina@teste.com.br', 'Carolina', '2000-03-20', 15000)
 
-INSERT INTO IMMOBILE (ID, ADDRESS, PICTURE, TYPE, VALUE) VALUES (100000, '22th street, 1800 - New York', 'picture.png', 'HOUSE', 1150000)
-INSERT INTO IMMOBILE (ID, ADDRESS, PICTURE, TYPE, VALUE) VALUES (100001, '15th avenue, 98 - New York', 'picture.png', 'HOUSE', 2150000)
-INSERT INTO IMMOBILE (ID, ADDRESS, PICTURE, TYPE, VALUE) VALUES (100002, 'Lincoln Avenue, 1200 - New York', 'picture.png', 'HOUSE', 3150000)
-INSERT INTO IMMOBILE (ID, ADDRESS, PICTURE, TYPE, VALUE) VALUES (100003, 'Spring Avenue, 100 - New York', 'picture.png', 'HOUSE', 4150000)
-INSERT INTO IMMOBILE (ID, ADDRESS, PICTURE, TYPE, VALUE) VALUES (100004, 'Washington Avenue, 3000 - New York', 'picture.png', 'HOUSE', 5150000)
+INSERT INTO IMMOBILE (ID, ADDRESS, PICTURE, TYPE, VALUE) VALUES (100000, '22th street, 1800 - New York', 'http://lorempixel.com/400/400/city/', 'HOUSE', 1150000)
+INSERT INTO IMMOBILE (ID, ADDRESS, PICTURE, TYPE, VALUE) VALUES (100001, '15th avenue, 98 - New York', 'http://lorempixel.com/400/400/city/', 'HOUSE', 2150000)
+INSERT INTO IMMOBILE (ID, ADDRESS, PICTURE, TYPE, VALUE) VALUES (100002, 'Lincoln Avenue, 1200 - New York', 'http://lorempixel.com/400/400/city/', 'HOUSE', 3150000)
+INSERT INTO IMMOBILE (ID, ADDRESS, PICTURE, TYPE, VALUE) VALUES (100003, 'Spring Avenue, 100 - New York', 'http://lorempixel.com/400/400/city/', 'HOUSE', 4150000)
+INSERT INTO IMMOBILE (ID, ADDRESS, PICTURE, TYPE, VALUE) VALUES (100004, 'Washington Avenue, 3000 - New York', 'http://lorempixel.com/400/400/city/', 'HOUSE', 5150000)
